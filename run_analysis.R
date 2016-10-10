3eaf17d843d2fbb258ce04f765d88f67c2733864
wscrdzg <yukuang.zhang@gmail.com>
2016-10-10 16:19:31 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -52,4 +52,6 @@ for(i in 1:6){
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the 
 #    average of each variable for each activity and each subject.
-mean_of_data <- aggregate(mean_n_std,list(mean_n_std$activity_type),mean)
+mean_of_data <- aggregate(mean_n_std[,-1],list(mean_n_std$activity_type),mean)
+
+write.table(mean_of_data, file = "tidy_data.txt", row.names = F)
