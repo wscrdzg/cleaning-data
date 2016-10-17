c6980c3034b34316630128f3940929a91d445b35
wscrdzg <yukuang.zhang@gmail.com>
2016-10-16 23:33:05 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,6 @@
+# the newest version is here:
+# https://github.com/wscrdzg/cleaning-data
+
 library(plyr)
 library(dplyr)
 
@@ -6,8 +9,11 @@ library(dplyr)
 feature <- read.table("./UCI_HAR_Dataset/features.txt")
 x_test <- read.table("./UCI_HAR_Dataset/test/X_test.txt")
 y_test <- read.table("./UCI_HAR_Dataset/test/y_test.txt")
+sub_test <- read.table("./UCI_HAR_Dataset/test/subject_test.txt")
 x_train <- read.table("./UCI_HAR_Dataset/train/X_train.txt")
 y_train <- read.table("./UCI_HAR_Dataset/train/y_train.txt")
+sub_train <- read.table("./UCI_HAR_Dataset/train/subject_train.txt")
+
 colnames(x_test) <- feature[,2]
 colnames(x_train) <- feature[,2]
 
@@ -21,8 +27,8 @@ colnames(test_or_train1) <- "test_0_train_1"
 # add that y_test and y_train, those are the activity label
 colnames(y_test) <- "activity_type"
 colnames(y_train) <- "activity_type"
-x_test1 <- cbind(y_test,test_or_train0,x_test)
-x_train1 <- cbind(y_train,test_or_train1,x_train)
+x_test1 <- cbind(sub_test,y_test,test_or_train0,x_test)
+x_train1 <- cbind(sub_train,y_train,test_or_train1,x_train)
 
 # combine test and train
 x_all <- rbind(x_test1,x_train1)
@@ -51,7 +57,7 @@ for(i in 1:6){
 ## already did in parts before
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the 
-#    average of each variable for each activity and each subject.
-mean_of_data <- aggregate(mean_n_std[,-1],list(mean_n_std$activity_type),mean)
-
+#    average of each variable for each activity and each subject. (Total 6*30 = 180 obs.)
+mean_n_std1 <- mean_n_std[,-2]
+mean_of_data <- aggregate(mean_n_std1,list(mean_n_std$V1,mean_n_std$activity_type),mean)
 write.table(mean_of_data, file = "tidy_data.txt", row.names = F)
